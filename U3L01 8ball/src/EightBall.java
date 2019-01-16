a8c64c9468f5acdc0b4d7e30fc703dc12acacff6
antonia <antonia@antonias-air.lan>
2019-01-16 08:18:06 -0500

    eight ball

--- U3L01 8ball/src/EightBall.java
+++ U3L01 8ball/src/EightBall.java
@@ -1,21 +1,47 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.UIManager;
 
 public class EightBall extends JFrame {
 
+	private JLabel messageLabel;
+	private JTextField question;
+	private String[] answers = {
+			 "It is certain.",
+			 "It is decidedly so.",
+			 "Without a doubt.",
+			 "Reply hazy, try again.",
+			 "Ask again later.",
+			 "Better not tell you now.",
+			 "Don't count on it.",
+			 "My reply is no.",
+			 "My sources say no."
+			
+			
+	};
+	
+	
+	
+	
 	public EightBall(){	
-		
+
+		setPreferredSize(new Dimension(800,220));
+
 		initGUI();
 		
 		setTitle("8 Ball");
-		setResizable(false);
+		setResizable(true);
 		pack(); //pack means pack tightly -- override the sizes 
 		setLocationRelativeTo(null); //centers on screen, do this after packing but before visible
 		setVisible(true);
@@ -24,7 +50,13 @@ public class EightBall extends JFrame {
 	
 	public void initGUI(){
 		
-		JLabel messageLabel = new JLabel("It is certain");
+		JTextField question = new JTextField();
+		question.setPreferredSize(new Dimension(800,80));
+		question.setHorizontalAlignment(JTextField.CENTER);
+		add(question,BorderLayout.PAGE_START);
+		
+		messageLabel = new JLabel("");
+		messageLabel.setPreferredSize(new Dimension(800,80));
 		//customize the label 
 		Font titlefount = new Font (Font.SERIF, Font.BOLD + Font.ITALIC, 64);
 		messageLabel.setFont(titlefount);
@@ -32,7 +64,7 @@ public class EightBall extends JFrame {
 		messageLabel.setBackground(new Color(3,27,197));
 		messageLabel.setOpaque(true);
 		
-		add(messageLabel,BorderLayout.PAGE_START);
+		add(messageLabel,BorderLayout.PAGE_END);
 		messageLabel.setHorizontalAlignment(JLabel.CENTER); //left or right
 		
 		JPanel bottomPanel = new JPanel();
@@ -42,9 +74,19 @@ public class EightBall extends JFrame {
 		shakeButton.setBackground(Color.black);
 		shakeButton.setForeground(Color.white);
 		shakeButton.setSize(50,50);
+		shakeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				shake();
+			}
+		});
 		bottomPanel.add(shakeButton,BorderLayout.CENTER);
 		
 		add(bottomPanel);
+	}
+	
+	public void shake() {
+		int answer = ((int)(Math.random() * (answers.length-1)) + 1);
+		messageLabel.setText(answers[answer]);		
 	}
 	
 	public static void main(String[] args) {
